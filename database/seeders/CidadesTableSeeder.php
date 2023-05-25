999bd932d2f8ff36bd3363a603e0a98a347a00aa
msrother <somenzi.rother@gmail.com>
2023-05-25 00:00:23 -0300

    Criação CidadesTableSeeder.

---
+++ database/seeders/CidadesTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Cidade;
+
+class CidadesTableSeeder extends Seeder
+{
+    /**
+     *  Para popular o banco, execute o comando: php artisan db:seed
+     */
+    public function run(Cidade $cidade)
+    {
+        $cidade->create([
+            'nome'=>'Passo Fundo',
+            'uf'  =>'RS',
+        ]);
+        
+        $cidade->create([
+            'nome'=>'Tapejara',
+            'uf'  =>'RS',
+        ]);
+
+        $cidade->create([
+            'nome'=>'Chapecó',
+            'uf'  =>'SC',
+        ]);
+
+        $cidade->create([
+            'nome'=>'Porto Alegre',
+            'uf'  =>'RS',
+        ]);
+    }
+}
+
